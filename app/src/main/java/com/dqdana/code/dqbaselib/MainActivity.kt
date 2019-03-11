579113281ccb3df7bac0a05a267ca767d5dc8bc5
DQ <dqdanavera@gmail.com>
2019-03-11 11:19:13 +0800

    feat: 更新 gradle 版本

--- app/src/main/java/com/dqdana/code/dqbaselib/MainActivity.kt
+++ app/src/main/java/com/dqdana/code/dqbaselib/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.dqdana.code.dqbaselib
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
